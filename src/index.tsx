3df819e349b124b1bf678ae799deb65d1b17ee85
tiagobega <62707329+tiagobega@users.noreply.github.com>
2024-09-19 20:53:59 -0300

    leva hidden

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { Leva } from 'leva';
 
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Leva hidden={!import.meta.env.DEV} />
   </React.StrictMode>
 );
 
